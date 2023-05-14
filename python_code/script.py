be7349cfa01db1604b8e974263d9facca2c6ee3c
Phan Cong Phong <phongdo.pbc@gmail.com>
2023-05-14 20:57:44 +0300

    edit README.md

--- python_code/script.py
+++ python_code/script.py
@@ -1,4 +1,4 @@
 import os
-# os.system('python -m pip install --upgrade pip')
+os.system('python -m pip install --upgrade pip')
 os.system('pip install -r requirements.txt')
 os.system('pyinstaller --onefile -w main.py')
