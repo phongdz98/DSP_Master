8700b00f9090f24b17970194e9649ed372e0947c
Phan Cong Phong <phongdo.pbc@gmail.com>
2023-05-25 12:02:32 +0300

    .

--- python_code/input/mysignal.py
+++ python_code/input/mysignal.py
@@ -3,5 +3,5 @@ import numpy as np
 fs = 8000
 ts = np.arange(0, 0.002, 1/fs)
 N = len(ts)
-x = np.sin(2*np.pi*1000*ts) + 0.9*np.sin(2*np.pi*500*ts + 3*np.pi/4)
+x = np.sin(2*np.pi*1000*ts) + 0.5*np.sin(2*np.pi*500*ts + 3*np.pi/4)
 np.savez('my_signal.npz', x=x, fs=fs)
